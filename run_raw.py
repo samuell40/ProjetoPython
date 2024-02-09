e5ba5146bca4bc3fca0ed099941e185dc85e60d7
samuel <samuelpc4567@gmail.com>
2024-02-09 21:31:00 +0000

    feat: Implementing View for tag creator with Http Types

--- run_raw.py
+++ run_raw.py
@@ -4,6 +4,7 @@ from barcode.writer import ImageWriter
 
 app = Flask(__name__)
 
+@app.route('/create_tag', methods=["POST"])
 def create_tag():
     body = request.json
     product_code = body.get('product_code')
@@ -13,4 +14,7 @@ def create_tag():
     tag.save(path_from_tag)
 
     return jsonify({ "tag_path": path_from_tag })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)
     
